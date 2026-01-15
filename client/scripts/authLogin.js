d62f0339625e1b2290e790ae75f8df3c180b8052
tpAnswer4yourself <antohaN1970@gmail.com>
2026-01-15 22:48:17 +0300

    переключение между формами, заглушки для сабмитов

--- client/scripts/authLogin.js
+++ client/scripts/authLogin.js
@@ -1,4 +1,37 @@
-//перехват submit
-//отправка fetch на /users/ для регистрации
-//отправка на /login для входа
-//сохранение токена в localStorage
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Страница загружена, можно начинать работу")
+
+    const form_login = document.getElementById("login-form")
+    const div_login = document.querySelector(".login_div")
+    const form_reg = document.getElementById("register-form")
+    const div_reg = document.querySelector(".reg_div")
+
+    const switch_to_reg = document.getElementById("switch-to-reg")
+    const switch_to_log = document.getElementById("switch-to-log")
+
+
+    const div_error = document.getElementById("error-message")
+
+    switch_to_reg.addEventListener('click', function(event) {
+        console.log("вход -> регистрация")
+        div_login.style.display = 'none'
+        div_reg.style.display = 'block'
+    })
+
+    switch_to_log.addEventListener('click', function(event) {
+        console.log("регистрация -> вход")
+        div_reg.style.display = 'none'
+        div_login.style.display = 'block'
+    })
+
+    form_login.addEventListener('submit', function(event) {
+        //перехват обработки отправки
+        event.preventDefault() //отмена стандартного поведения формы (перезагрузка страницы)
+        console.log("form-login форма обрабатывается")
+    })
+
+    form_reg.addEventListener('submit', function(event) {
+        event.preventDefault()
+        console.log("form-reg форма обрабатывается")
+    })
+})
